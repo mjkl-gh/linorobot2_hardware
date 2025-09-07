5ffd84c1f5193b88623cf6007b6eb7ea72a6eba9
mjkl-gh <9350991+mjkl-gh@users.noreply.github.com>
2025-09-07 17:38:54 +0200

    refactor: Remove unrequired configs

--- config/config.h
+++ config/config.h
@@ -14,14 +14,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#ifdef USE_VATTENKAR_CONFIG
-    #include "custom/vattenkar_config.h"
-#endif
-
-#ifdef USE_DEV_CONFIG
-    #include "custom/dev_config.h"
-#endif
-
 #ifdef USE_GENDRV_CONFIG
     #include "custom/gendrv_config.h"
 #endif
@@ -30,45 +22,6 @@
     #include "custom/gendrv_wifi_config.h"
 #endif
 
-#ifdef USE_ESP32_CONFIG
-    #include "custom/esp32_config.h"
-#endif
-
-#ifdef USE_ESP32_WIFI_CONFIG
-    #include "custom/esp32_wifi_config.h"
-#endif
-
-#ifdef USE_ESP32S2_CONFIG
-    #include "custom/esp32s2_config.h"
-#endif
-
-#ifdef USE_ESP32S2_WIFI_CONFIG
-    #include "custom/esp32s2_wifi_config.h"
-#endif
-
-#ifdef USE_ESP32S3_CONFIG
-    #include "custom/esp32s3_config.h"
-#endif
-
-#ifdef USE_ESP32S3_WIFI_CONFIG
-    #include "custom/esp32s3_wifi_config.h"
-#endif
-
-#ifdef USE_PICO2_CONFIG
-    #include "custom/pico2_config.h"
-#endif
-
-#ifdef USE_PICO_CONFIG
-    #include "custom/pico_config.h"
-#endif
-
-// add maintainer configurations above this line
-// this barrier helps to reduce user merge conflict
-// add user configurations below this line
-
-
-// add user configurations above this line
-// this should be the last one
 #ifndef LINO_BASE
     #include "lino_base_config.h"
 #endif
